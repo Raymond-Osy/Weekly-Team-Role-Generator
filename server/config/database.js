147b540179418cc21cb3a5834970263c321c7c6f
Oluwaseyi Adebajo <oluwaseyiadebajo@Oluwaseyis-MacBook-Pro.local>
2019-03-22 15:16:30 +0100

    adjusting my  database  credential

--- server/config/database.js
+++ server/config/database.js
@@ -1,34 +1,33 @@
-
-const { 
+const {
   PROD_DB_USER,
   PROD_DB_NAME,
   PROD_DB_PASSWORD,
   PROD_DB_HOST
 } = process.env;
 
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "username": "micahakpan",
-    "password": "",
-    "database": "team-cosmos-db",
-    "host": "127.0.0.1",
-    'port': 5432,
-    "dialect": "postgres"
+  development: {
+    username: "postgres",
+    password: "a1b2c3d4e5",
+    database: "team-cosmos-db",
+    host: "127.0.0.1",
+    port: 5432,
+    dialect: "postgres"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "production": {
-    "username":  PROD_DB_USER,
-    "password": PROD_DB_PASSWORD,
-    "database":  PROD_DB_NAME,
-    "host":  PROD_DB_HOST,
-    "dialect": "postgres"
+  production: {
+    username: PROD_DB_USER,
+    password: PROD_DB_PASSWORD,
+    database: PROD_DB_NAME,
+    host: PROD_DB_HOST,
+    dialect: "postgres"
   }
-}
+};
