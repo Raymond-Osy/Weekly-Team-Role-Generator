295f7709ae321e3539b1ff64910517e01ffc8e40
Micah Akpan <mickey4sure2k7@gmail.com>
2019-03-22 18:10:43 +0100

    Refactor code + Enhance UX

--- frontend/js/generate-random.js
+++ frontend/js/generate-random.js
@@ -4,6 +4,7 @@ const baseApiUrl = 'https://hidden-hollows-71371.herokuapp.com/api';
 const scrumMaster = document.getElementById('crum-master');
 const qa1 = document.getElementById("q1");
 const qa2 = document.getElementById("q2");
+const generateRoleButton = document.getElementById("generate");
 
 /**
  * @func getTeamMemberFullName
@@ -35,9 +36,30 @@ const generateTeamRoles = () => {
             currentweekRole.push(randomMember);
           }
         }
-        scrumMaster.innerHTML = `${getTeamMemberFullName(currentweekRole[0])}`;
-        qa1.innerHTML = `${getTeamMemberFullName(currentweekRole[1])}`;
-        qa2.innerHTML = `${getTeamMemberFullName(currentweekRole[2])}`;
+
+        const [ teamLead, firstQA, secondQA ] = currentweekRole;
+
+        // Update Role and Served Status
+        fetch(`${baseApiUrl}/users/${teamLead.id}`, {
+          method: 'PATCH',
+          body: JSON.stringify({
+            served: true,
+            endDate: new Date(new Date().getTime - 24 * 60 * 60 * 1000 * 4)
+          })
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((err) => {
+            
+          })
+
+        scrumMaster.innerHTML = `${getTeamMemberFullName(teamLead)}`;
+        qa1.innerHTML = `${getTeamMemberFullName(firstQA)}`;
+        qa2.innerHTML = `${getTeamMemberFullName(secondQA)}`;
         
       } else {
         console.log("server connection failed");
@@ -46,7 +68,44 @@ const generateTeamRoles = () => {
     .catch(error => console.log(error));
 };
 
+generateRoleButton.addEventListener("click", () => generateTeamRoles());
 
-document
-  .getElementById("generate")
-  .addEventListener("click", () => generateTeamRoles());
+/**
+ * @func anyActiveTeamLead
+ * @param {Array} members 
+ * @param {Array}
+ */
+const anyActiveTeamLead = (members) => {
+  const pastTeamLeads = members.filter((member) => member.served);
+  const activeTeamLead = pastTeamLeads.filter((teamLead) => new Date().getTime() - new Date(teamLead.endDate).getTime() > 0);
+  return activeTeamLead;
+}
+
+/**
+ * @func getAllTeamMembers
+ * @returns {Promise<Array>} Returns a promise that resolves to an  array
+ */
+const getAllTeamMembers = () => {
+  return fetch(`${baseApiUrl}/users`)
+    .then(response => response.json())
+    .then((data) => {
+      return data.data;
+    })
+    .catch((err) => {
+      throw err;
+    })
+}
+
+window.onload = () => {
+  // if team lead already exist for the week and is active
+  // show team lead
+  // do the same for QA
+  getAllTeamMembers()
+   .then((members) => anyActiveTeamLead(members))
+   .then((member) => {
+     console.log(member);
+   })
+   .catch((err) => {
+     // TODO: Handle network error here
+   })
+}
